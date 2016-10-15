d3e0803368eb0c6b8fa88a022d9a2a7f8b6cba92
Franklyn Roth <franklyn.roth@markit.com>
2016-10-14 18:57:13 -0600

    Update styles

---
+++ script.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    //saving dom objects to variables
+    var container = $('#container');
+    var bird = $('#bird');
+    var pole = $('.pole');
+    var pole_1 = $('#pole_1');
+    var pole_2 = $('#pole_2');
+    var score = $('#score');
+    var speed_span = $('#speed');
+    var restart_btn = $('#restart_btn');
+
+    //saving some initial setup
+    var container_width = parseInt(container.width());
+    var container_height = parseInt(container.height());
+    var pole_initial_position = parseInt(pole.css('right'));
+    var pole_initial_height = parseInt(pole.css('height'));
+    var bird_left = parseInt(bird.css('left'));
+    var bird_height = parseInt(bird.height());
+    var speed = 10;
+ 
+});
