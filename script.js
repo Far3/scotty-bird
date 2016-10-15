bb2bb9948a3efdec389af8e0c19a4d2571c1f2cd
Franklyn Roth <franklyn.roth@markit.com>
2016-10-14 19:25:11 -0600

    Add index back in

---
+++ script.js
@@ -0,0 +1,78 @@
+$(function () {
+
+    //saving dom objects to variables
+    var container = $('#container');
+    var bird = $('#bird');
+    var pole = $('.pole');
+    var pole_1 = $('#pole_1');
+    var pole_2 = $('#pole_2');
+    var score = $('#score');
+    var speed_span = $('#speed');
+    var restart_btn = $('#restart_btn');
+
+    //saving some initial setup
+    var container_width = parseInt(container.width());
+    var container_height = parseInt(container.height());
+    var pole_initial_position = parseInt(pole.css('right'));
+    var pole_initial_height = parseInt(pole.css('height'));
+    var bird_left = parseInt(bird.css('left'));
+    var bird_height = parseInt(bird.height());
+    var speed = 50;
+    speed_span.text(speed);
+
+    //other declarations
+    var go_up = false;
+
+    var the_game = setInterval(function () {
+
+        var pole_current_position = parseInt(pole.css('right'));
+        
+        //check whether the poles went out of the container or not
+        if (pole_current_position > container_width) {
+            
+            var new_height = parseInt(Math.random() * 100);
+            console.log(new_height);
+
+            //Change the poles height
+            pole_1.css('height', pole_initial_height + new_height);
+            pole_2.css('height', pole_initial_height - new_height);
+
+            //increase speed
+            speed = speed + 5;
+            speed_span.text(speed);
+            
+            pole_current_position = pole_initial_position;
+        }
+
+        //move the poles
+        pole.css('right', pole_current_position + speed);
+
+        if (go_up === false) {
+            go_down();
+        }
+    }, 40);
+
+    $(document).on('keydown', function(e){
+        var key = e.keyCode;
+        if(key === 32 && go_up === false){
+            go_up = setInterval(up, 25);
+        }
+    });
+
+    $(document).on('keyup', function(e){
+        var key = e.keyCode;
+        if(key === 32){
+            clearInterval(go_up)
+            go_up = false;
+        }
+    });
+
+    function up() {
+        bird.css('top', parseInt(bird.css('top')) - 5);
+    }
+    
+    function go_down() {
+        bird.css('top', parseInt(bird.css('top')) + 5);
+    }
+ 
+});
